612ed96829707331b7b41f3b16caf24aa6581124
Rob Applin <robert.applin@stfc.ac.uk>
2018-08-02 11:41:32 +0100

    refs #23144 Set store in ADS false - IndirectDataReduction

--- qt/scientific_interfaces/Indirect/IndirectDataReductionTab.cpp
+++ qt/scientific_interfaces/Indirect/IndirectDataReductionTab.cpp
@@ -150,7 +150,7 @@ std::map<std::string, double> IndirectDataReductionTab::getRangesFromInstrument(
 
   IAlgorithm_sptr createWsAlg =
       AlgorithmManager::Instance().create("CreateWorkspace");
-  createWsAlg->setChild(true);
+  createWsAlg->setAlwaysStoreInADS(false);
   createWsAlg->initialize();
   createWsAlg->setProperty("OutputWorkspace", "__energy");
   createWsAlg->setProperty("DataX", x);
@@ -163,7 +163,7 @@ std::map<std::string, double> IndirectDataReductionTab::getRangesFromInstrument(
 
   IAlgorithm_sptr convertHistAlg =
       AlgorithmManager::Instance().create("ConvertToHistogram");
-  convertHistAlg->setChild(true);
+  convertHistAlg->setAlwaysStoreInADS(false);
   convertHistAlg->initialize();
   convertHistAlg->setProperty("InputWorkspace", energyWs);
   convertHistAlg->setProperty("OutputWorkspace", "__energy");
@@ -172,7 +172,7 @@ std::map<std::string, double> IndirectDataReductionTab::getRangesFromInstrument(
 
   IAlgorithm_sptr loadInstAlg =
       AlgorithmManager::Instance().create("LoadInstrument");
-  loadInstAlg->setChild(true);
+  loadInstAlg->setAlwaysStoreInADS(false);
   loadInstAlg->initialize();
   loadInstAlg->setProperty("Workspace", energyWs);
   loadInstAlg->setProperty("InstrumentName", instName.toStdString());
@@ -188,7 +188,7 @@ std::map<std::string, double> IndirectDataReductionTab::getRangesFromInstrument(
 
   IAlgorithm_sptr loadParamAlg =
       AlgorithmManager::Instance().create("LoadParameterFile");
-  loadParamAlg->setChild(true);
+  loadParamAlg->setAlwaysStoreInADS(false);
   loadParamAlg->initialize();
   loadParamAlg->setProperty("Workspace", energyWs);
   loadParamAlg->setProperty("Filename", ipfFilename.toStdString());
@@ -208,7 +208,7 @@ std::map<std::string, double> IndirectDataReductionTab::getRangesFromInstrument(
 
   IAlgorithm_sptr convUnitsAlg =
       AlgorithmManager::Instance().create("ConvertUnits");
-  convUnitsAlg->setChild(true);
+  convUnitsAlg->setAlwaysStoreInADS(false);
   convUnitsAlg->initialize();
   convUnitsAlg->setProperty("InputWorkspace", energyWs);
   convUnitsAlg->setProperty("OutputWorkspace", "__tof");
