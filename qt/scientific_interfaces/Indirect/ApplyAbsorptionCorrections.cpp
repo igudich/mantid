1325f8d6d50614b53c137f624f00340b91d7fa57
Rob <robertgjapplin@gmail.com>
2018-10-14 12:39:39 +0100

    Refs #23757. Add Plot spectrum option

--- qt/scientific_interfaces/Indirect/ApplyAbsorptionCorrections.cpp
+++ qt/scientific_interfaces/Indirect/ApplyAbsorptionCorrections.cpp
@@ -10,6 +10,7 @@
 #include "MantidAPI/Run.h"
 #include "MantidAPI/TextAxis.h"
 #include "MantidAPI/WorkspaceGroup.h"
+#include "MantidQtWidgets/Common/SignalBlocker.h"
 
 #include <QStringList>
 
@@ -46,7 +47,10 @@ ApplyAbsorptionCorrections::ApplyAbsorptionCorrections(QWidget *parent)
           SLOT(updateContainer()));
   connect(m_uiForm.ckUseCan, SIGNAL(toggled(bool)), this,
           SLOT(updateContainer()));
-  connect(m_uiForm.pbPlot, SIGNAL(clicked()), this, SLOT(plotClicked()));
+  connect(m_uiForm.pbPlotSpectrum, SIGNAL(clicked()), this,
+          SLOT(plotSpectrumClicked()));
+  connect(m_uiForm.pbPlotContour, SIGNAL(clicked()), this,
+          SLOT(plotContourClicked()));
   connect(m_uiForm.pbSave, SIGNAL(clicked()), this, SLOT(saveClicked()));
   connect(m_uiForm.pbRun, SIGNAL(clicked()), this, SLOT(runClicked()));
   connect(m_uiForm.pbPlotPreview, SIGNAL(clicked()), this,
@@ -346,6 +350,15 @@ void ApplyAbsorptionCorrections::run() {
   // updateContainer();
 }
 
+std::size_t ApplyAbsorptionCorrections::getOutWsNumberOfSpectra() const {
+  return getADSWorkspace(m_pythonExportWsName)->getNumberHistograms();
+}
+
+MatrixWorkspace_const_sptr
+ApplyAbsorptionCorrections::getADSWorkspace(std::string const &name) const {
+  return AnalysisDataService::Instance().retrieveWS<MatrixWorkspace>(name);
+}
+
 /**
  * Adds a spline interpolation as a step in the calculation for using legacy
  *correction factor
@@ -400,7 +413,8 @@ void ApplyAbsorptionCorrections::absCorComplete(bool error) {
     m_batchAlgoRunner->executeBatchAsync();
   } else {
     setRunIsRunning(false);
-    setPlotResultEnabled(false);
+    setPlotSpectrumEnabled(false);
+    setPlotContourEnabled(false);
     setSaveResultEnabled(false);
     emit showMessageBox(
         "Unable to apply corrections.\nSee Results Log for more details.");
@@ -418,6 +432,8 @@ void ApplyAbsorptionCorrections::postProcessComplete(bool error) {
   setRunIsRunning(false);
 
   if (!error) {
+    setPlotSpectrumIndexMax(static_cast<int>(getOutWsNumberOfSpectra()) - 1);
+
     // Handle preview plot
     plotPreview(m_uiForm.spPreviewSpec->value());
 
@@ -437,7 +453,8 @@ void ApplyAbsorptionCorrections::postProcessComplete(bool error) {
       deleteAlg->execute();
     }
   } else {
-    setPlotResultEnabled(false);
+    setPlotSpectrumEnabled(false);
+    setPlotContourEnabled(false);
     setSaveResultEnabled(false);
     emit showMessageBox("Unable to process corrected workspace.\nSee Results "
                         "Log for more details.");
@@ -560,19 +577,23 @@ void ApplyAbsorptionCorrections::saveClicked() {
   m_batchAlgoRunner->executeBatchAsync();
 }
 
-void ApplyAbsorptionCorrections::plotClicked() {
-  setPlotResultIsPlotting(true);
+void ApplyAbsorptionCorrections::plotSpectrumClicked() {
+  setPlotSpectrumIsPlotting(true);
 
-  QString plotType = m_uiForm.cbPlotOutput->currentText();
-  if (checkADSForPlotSaveWorkspace(m_pythonExportWsName, true)) {
+  auto const spectrumIndex = m_uiForm.spSpectrum->text().toInt();
+  if (checkADSForPlotSaveWorkspace(m_pythonExportWsName, true))
+    plotSpectrum(QString::fromStdString(m_pythonExportWsName), spectrumIndex);
 
-    if (plotType == "Spectra" || plotType == "Both")
-      plotSpectrum(QString::fromStdString(m_pythonExportWsName));
+  setPlotSpectrumIsPlotting(false);
+}
 
-    if (plotType == "Contour" || plotType == "Both")
-      plot2D(QString::fromStdString(m_pythonExportWsName));
-  }
-  setPlotResultIsPlotting(false);
+void ApplyAbsorptionCorrections::plotContourClicked() {
+  setPlotContourIsPlotting(true);
+
+  if (checkADSForPlotSaveWorkspace(m_pythonExportWsName, true))
+    plot2D(QString::fromStdString(m_pythonExportWsName));
+
+  setPlotContourIsPlotting(false);
 }
 
 void ApplyAbsorptionCorrections::runClicked() { runTab(); }
@@ -638,13 +659,26 @@ void ApplyAbsorptionCorrections::plotInPreview(const QString &curveName,
   }
 }
 
+void ApplyAbsorptionCorrections::setPlotSpectrumIndexMax(int maximum) {
+  MantidQt::API::SignalBlocker<QObject> blocker(m_uiForm.spSpectrum);
+  m_uiForm.spSpectrum->setMaximum(maximum);
+}
+
+int ApplyAbsorptionCorrections::getPlotSpectrumIndex() {
+  return m_uiForm.spSpectrum->text().toInt();
+}
+
 void ApplyAbsorptionCorrections::setRunEnabled(bool enabled) {
   m_uiForm.pbRun->setEnabled(enabled);
 }
 
-void ApplyAbsorptionCorrections::setPlotResultEnabled(bool enabled) {
-  m_uiForm.pbPlot->setEnabled(enabled);
-  m_uiForm.cbPlotOutput->setEnabled(enabled);
+void ApplyAbsorptionCorrections::setPlotSpectrumEnabled(bool enabled) {
+  m_uiForm.pbPlotSpectrum->setEnabled(enabled);
+  m_uiForm.spSpectrum->setEnabled(enabled);
+}
+
+void ApplyAbsorptionCorrections::setPlotContourEnabled(bool enabled) {
+  m_uiForm.pbPlotContour->setEnabled(enabled);
 }
 
 void ApplyAbsorptionCorrections::setSaveResultEnabled(bool enabled) {
@@ -654,13 +688,23 @@ void ApplyAbsorptionCorrections::setSaveResultEnabled(bool enabled) {
 void ApplyAbsorptionCorrections::setRunIsRunning(bool running) {
   m_uiForm.pbRun->setText(running ? "Running..." : "Run");
   setRunEnabled(!running);
-  setPlotResultEnabled(!running);
+  setPlotSpectrumEnabled(!running);
+  setPlotContourEnabled(!running);
   setSaveResultEnabled(!running);
 }
 
-void ApplyAbsorptionCorrections::setPlotResultIsPlotting(bool plotting) {
-  m_uiForm.pbPlot->setText(plotting ? "Plotting..." : "Plot");
-  setPlotResultEnabled(!plotting);
+void ApplyAbsorptionCorrections::setPlotSpectrumIsPlotting(bool plotting) {
+  m_uiForm.pbPlotSpectrum->setText(plotting ? "Plotting..." : "Plot Spectrum");
+  setPlotSpectrumEnabled(!plotting);
+  setPlotContourEnabled(!plotting);
+  setRunEnabled(!plotting);
+  setSaveResultEnabled(!plotting);
+}
+
+void ApplyAbsorptionCorrections::setPlotContourIsPlotting(bool plotting) {
+  m_uiForm.pbPlotContour->setText(plotting ? "Plotting..." : "Plot Contour");
+  setPlotContourEnabled(!plotting);
+  setPlotSpectrumEnabled(!plotting);
   setRunEnabled(!plotting);
   setSaveResultEnabled(!plotting);
 }
